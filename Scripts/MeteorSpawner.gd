39774a3e9f0c896d4dbdf4f60ffda0bbadbb2209
Vitor Rodrigues <vitor.n.rodrigues@gmail.com>
2019-06-03 02:22:00 -0300

    Add powerup to the MeteorSpawner

--- Scripts/MeteorSpawner.gd
+++ Scripts/MeteorSpawner.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var meteor_preload = preload("res://Prefabs/Meteor.tscn")
+var powerup_preload = preload("res://Prefabs/PowerUp.tscn")
 var wait_time = 2
 
 func _ready():
@@ -12,8 +13,17 @@ func _process(delta):
 	if wait_time > 0:
 		wait_time -= delta
 	else:
-		spawnMeteor()
-
+		if (randi() % 100 >= 75):
+			spawnPowerUp()
+		else:
+			spawnMeteor()
+			
+func spawnPowerUp():
+	var pup = powerup_preload.instance()
+	var maxX = get_viewport_rect().end.x - 30
+	pup.position = Vector2(rand_range(30, maxX), -60)
+	get_parent().add_child(pup)
+	
 func spawnMeteor():
 	wait_time = rand_range(0.3, 1)
 	var meteor = meteor_preload.instance()
--- Scripts/PowerUp.gd
+++ Scripts/PowerUp.gd
@@ -1,7 +1,17 @@
 extends Area2D
 
 export var value = 2
+var speed = 150
+func _process(delta):
+	position += Vector2(0, speed) * delta
+	
+	if position.y > get_viewport_rect().end.y - 30:
+		remove()
+	
 
 func _on_Area2D_area_entered(area):
 	area.change_weapon(value)
+	remove()
+
+func remove():
 	queue_free()
