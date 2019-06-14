75fb99566ecfa7db054e2a5483ff012537875b78
Vitor Rodrigues <vitor.n.rodrigues@gmail.com>
2019-06-14 02:11:47 -0300

    Add enemy sounds for shot and explosion

--- EnemyShip.gd
+++ EnemyShip.gd
@@ -12,6 +12,7 @@ func fire():
 	var shot = fire.instance()
 	shot.global_position = $GunPosition.global_position
 	Game.get_camera().add_child(shot)
+	$shot_audio.play()
 
 func _on_Timer_timeout():
 	fire()
@@ -21,5 +22,6 @@ func destroy():
 	$Timer.stop()
 	$Explosion.show()
 	$Explosion/Anim.play("Explode")
+	$explosion_audio.play()
 	emit_signal("ship_destroyed")
 	
